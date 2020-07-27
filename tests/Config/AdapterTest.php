08049f053a675c499a8d28c045702ce4862eb479
Ben Selby <benmatselby@gmail.com>
2020-07-27 19:04:03 +0100

    Add some extra tests for the config adapter

--- tests/Config/AdapterTest.php
+++ tests/Config/AdapterTest.php
@@ -5,12 +5,33 @@ namespace TrelloCli\Test\Config;
 use TrelloCli\Config\Adapter;
 
 /**
- * Trello Config Adapter Test Class
- *
  * Responsible for testing \TrelloCli\Config\Adapter
  */
 class AdapterTest extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * @inheritdoc
+     */
+    public function tearDown(): void
+    {
+        putenv("TRELLO_CLI_KEY");
+        putenv("TRELLO_CLI_SECRET");
+    }
+
+    /**
+     * @covers \TrelloCli\Config\Adapter::getConfig
+     */
+    public function testTheDefaultIsEnvironment(): void
+    {
+        putenv("TRELLO_CLI_KEY=key");
+        putenv("TRELLO_CLI_SECRET=secret");
+        $config = Adapter::getConfig();
+
+        $this->assertInstanceOf('\TrelloCli\Config', $config);
+        $this->assertEquals($config->getKey(), "key");
+        $this->assertEquals($config->getSecret(), "secret");
+    }
+
     /**
      * @covers \TrelloCli\Config\Adapter::getConfig
      */
@@ -19,5 +40,7 @@ class AdapterTest extends \PHPUnit\Framework\TestCase
         $config = Adapter::getConfig();
 
         $this->assertInstanceOf('\TrelloCli\Config', $config);
+        $this->assertNull($config->getKey());
+        $this->assertNull($config->getSecret());
     }
 }
