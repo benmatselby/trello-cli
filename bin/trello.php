1c9efb5655f4607c4fb84ad03412a233d1af9412
Ben Selby <benmatselby@gmail.com>
2026-03-03 20:53:22 +0000

    Upgrade Symfony Console

--- bin/trello.php
+++ bin/trello.php
@@ -8,11 +8,11 @@ $client = \TrelloCli\Client::instance(\TrelloCli\Config\Adapter::getConfig());
 use Symfony\Component\Console\Application;
 
 $app = new Application('Trello CLI');
-$app->add(new \TrelloCli\Command\ListCardsCommand($client));
-$app->add(new \TrelloCli\Command\ListBoardsCommand($client));
-$app->add(new \TrelloCli\Command\ListPeopleCommand($client));
-$app->add(new \TrelloCli\Command\LabelCardsCommand($client));
-$app->add(new \TrelloCli\Command\JsonExportBoardCommand($client));
-$app->add(new \TrelloCli\Command\BurndownCommand($client));
-$app->add(new \TrelloCli\Command\SortCommand($client));
+$app->addCommand(new \TrelloCli\Command\ListCardsCommand($client));
+$app->addCommand(new \TrelloCli\Command\ListBoardsCommand($client));
+$app->addCommand(new \TrelloCli\Command\ListPeopleCommand($client));
+$app->addCommand(new \TrelloCli\Command\LabelCardsCommand($client));
+$app->addCommand(new \TrelloCli\Command\JsonExportBoardCommand($client));
+$app->addCommand(new \TrelloCli\Command\BurndownCommand($client));
+$app->addCommand(new \TrelloCli\Command\SortCommand($client));
 $app->run();
